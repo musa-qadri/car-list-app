24fc1e1e070237f23c21e007c89a60058baf4aa8
Musa Qadri <musaqazim1@gmail.com>
2024-02-22 23:57:57 +0500

    Initial commit

---
+++ app.js
@@ -0,0 +1,513 @@
+var carlist = {
+    suzuki : {
+        mehran : {
+            image:'https://www.suzukipremier.com/wp-content/uploads/2020/10/Suzuki_Mehran_2012.jpg',
+            model:'2020',
+            seatcapacity:'5 person',
+            fuleType : 'petrol',
+            Transmission :'Type	Manual',
+            Mileage: "14 to 18 KM/LITER",
+            Wheel :	'12 in',
+            price: '1,700,000 PKR',
+            color: ['red','white','silver']   
+        },
+        alto :{
+            image:'https://suzukifortmotors.com/wp-content/uploads/2020/12/Alto-Pearl-Black.jpg',
+            model:'2022',
+            seatcapacity:'5 person',
+            fuleType : 'petrol',
+            Transmission :'Type	Manual',
+            Mileage: "17 to 22 KM/LITER",
+            Wheel :	'14 in',
+            price: '2,400,000 PKR',
+            color: ['red','white','black']   
+  
+        
+        },
+        cultus :{
+            image:'https://imgcdn.zigwheels.pk/large/gallery/color/13/124/suzuki-cultus-color-704737.jpg',
+            model:'2021',
+            seatcapacity:'5 person',
+            fuleType : 'petrol',
+            Transmission :'Type	Auto',
+            Mileage: "15 to 19 KM/LITER",
+            Wheel :	'14 in',
+            price: '2,900,000 PKR',  
+            color: ['blue','white','silver']   
+
+        
+        },
+        swift :{
+            image:'https://media.umbraco.io/suzuki-gb/axph1a1m/8636_suzuki_swift_sz-t_lf_r5_mg.png',
+            model:'2023',
+            seatcapacity:'5 person',
+            fuleType : 'petrol',
+            Transmission :'Type	Auto',
+            Mileage: "15 to 19 KM/LITER",
+            Wheel :	'14 in',
+            price: '4,200,000 PKR',  
+            color: ['red','white','black']   
+
+
+        },
+        vegonR : {
+            image:'https://imgd.aeplcdn.com/1920x1080/n/cw/ec/112947/wagon-r-2022-exterior-right-front-three-quarter-2.jpeg?isig=0&q=80&q=80',
+            model:'2023',
+            seatcapacity:'5 person',
+            fuleType : 'petrol',
+            Transmission :'Type	Auto',
+            Mileage: "15 to 19 KM/LITER",
+            Wheel :	'14 in',
+            price: '3,200,000 PKR',  
+            color: ['red','white','black']   
+
+
+        }
+        
+    },
+    honda :{
+        city : {
+            image:'https://propakistani.pk/wp-content/uploads/2021/05/Honda-City-2017-Present_8c605f21-5d66-4bd4-a82d-c9f62a0a8a82-e1620197316487.jpg',
+            model:'2023',
+            seatcapacity:'5 person',
+            fuleType : 'petrol',
+            Transmission :'Type	Automatic',
+            Mileage: "15 to 19 KM/LITER",
+            Wheel :	'14 in',
+            price: '4,200,000 PKR',  
+            color: ['red','white','black']   
+        },
+        BRV :{
+            image:'https://www.checkraka.com/uploaded/gallery/7e/7edc4e0d8f620ad3e1e3f7cb788119eb.jpg',
+            model:'2023',
+            seatcapacity:'5 person',
+            fuleType : 'petrol',
+            Transmission :'Type	Auto',
+            Mileage: "15 to 19 KM/LITER",
+            Wheel :	'14 in',
+            price: '6,200,000 PKR',  
+            color: ['red','white','black']   
+        },
+        accord :{
+            image:'https://edgecast-img.yahoo.net/mysterio/api/D9BC91C6C5FB2E79A38806A3E57B7A23C867841A44C5314E3ED04F0822E7A7F9/autoblog/resizefill_w660_h372;quality_80;format_webp;cc_31536000;/https://s.aolcdn.com/commerce/autodata/images/USC90HOC011A021001.jpg',
+            model:'2023',
+            seatcapacity:'5 person',
+            fuleType : 'petrol',
+            Transmission :'Type	Auto',
+            Mileage: "15 to 19 KM/LITER",
+            Wheel :	'22 in',
+            price: '22,346,000 PKR',  
+            color: ['red','white','black']   
+        },
+        civic :{
+            image:'https://honda.com.pk/images/2022/03/3.jpg',
+            model:'2023',
+            seatcapacity:'5 person',
+            fuleType : 'petrol',
+            Transmission :'Type	Auto',
+            Mileage: "15 to 19 KM/LITER",
+            Wheel :	'18 in',
+            price: '8,329,000 PKR',  
+            color: ['red','white','black']   
+        },
+        HRV :{
+            image:'https://i0.wp.com/carandbike.pk/wp-content/uploads/2023/02/hrv-honda-price.png?fit=589%2C424&ssl=1',
+            model:'2023',
+            seatcapacity:'5 person',
+            fuleType : 'petrol',
+            Transmission :'Type	Auto',
+            Mileage: "15 to 19 KM/LITER",
+            Wheel :	'16 in',
+            price: '7,649,000 PKR',  
+            color: ['red','white','black']   
+        },
+        CRV :{
+            image:'https://mystrongad.com/BPH_BillPageHonda/Digital/CR-V/2023/CAR%20CUTS/WEBP/23-Honda-CR-V-Platinum-White-Pearl.webp',
+            model:'2023',
+            seatcapacity:'5 person',
+            fuleType : 'petrol',
+            Transmission :'Type	Auto',
+            Mileage: "15 to 19 KM/LITER",
+            Wheel :	'14 in',
+            price: '8,658,000 PKR',  
+            color: ['red','white','black']   
+        }
+
+
+    },
+    toyota : {
+        corolla :{
+            image:'https://imgcdn.zigwheels.pk/large/gallery/exterior/14/118/toyota-corolla-front-angle-low-view.jpg',
+            model:'2023',
+            seatcapacity:'5 person',
+            fuleType : 'petrol',
+            Transmission :'Type	Auto',
+            Mileage: "15 to 19 KM/LITER",
+            Wheel :	'16 in',
+            price: '5,960,000 PKR',  
+            color: ['red','white','black']   
+        },
+        yaris :{
+            image:'https://toyota-central.com/Assets/images/Vehicle/Yaris/Color/attitude-black.png',
+            model:'2023',
+            seatcapacity:'5 person',
+            fuleType : 'petrol',
+            Transmission :'Type	Auto',
+            Mileage: "15 to 19 KM/LITER",
+            Wheel :	'14 in',
+            price: '4,320,000 PKR',  
+            color: ['red','white','black']   
+        },
+        camry :{
+            image:'https://www.royaltour.in/wp-content/uploads/2022/06/royaltour_camray.webp',
+            model:'2023',
+            seatcapacity:'5 person',
+            fuleType : 'petrol',
+            Transmission :'Type	Auto',
+            Mileage: "15 to 19 KM/LITER",
+            Wheel :	'14 in',
+            price: '23,200,000 PKR',  
+            color: ['red','white','black']   
+        },
+        premio :{
+            image:'https://catalogphoto.goo-net.com/carphoto/10101057_201004s.jpg',
+            model:'2023',
+            seatcapacity:'5 person',
+            fuleType : 'petrol',
+            Transmission :'Type	Automatic',
+            Mileage: "15 to 19 KM/LITER",
+            Wheel :	'14 in',
+            price: '13,200,000 PKR',  
+            color: ['red','white','black']   
+        },
+        landcruiser :{
+            image:'https://global.toyota/pages/news/images/2021/08/02/1330/002.jpg',
+            model:'2023',
+            seatcapacity:'8 person',
+            fuleType : 'petrol',
+            Transmission :'Type	Auto',
+            Mileage: "10 to 12 KM/LITER",
+            Wheel :	'19 in',
+            price: '23,200,000 PKR',  
+            color: ['red','white','black']   
+        },
+        crollacross :{
+            image:'https://cache4.pakwheels.com/system/car_generation_pictures/7493/original/20200709104304_ac2.jpg?1685107928',
+            model:'2023',
+            seatcapacity:'5 person',
+            fuleType : 'petrol',
+            Transmission :'Type	Auto',
+            Mileage: "12 to 16 KM/LITER",
+            Wheel :	'16 in',
+            price: '9,399,000 PKR',  
+            color: ['red','white','black']   
+        },
+        landcruiser300 :{
+            image:'https://global.toyota/pages/news/images/2021/08/02/1330/001.jpg',
+            model:'2023',
+            seatcapacity:'8 person',
+            fuleType : 'petrol',
+            Transmission :'Type	Auto',
+            Mileage: "115 to 1 KM/LITER",
+            Wheel :	'18 in',
+            price: '33,200,000 PKR',  
+            color: ['red','white','black']   
+        },
+        fortuner :{
+            image:'https://cache4.pakwheels.com/system/car_generation_pictures/7335/original/Fortuner_-_PNG.png?1677568997',
+            model:'2023',
+            seatcapacity:'8 person',
+            fuleType : 'petrol',
+            Transmission :'Type	Auto',
+            Mileage: "15 to 19 KM/LITER",
+            Wheel :	'18 in',
+            price: '14,499,000 PKR',  
+            color: ['red','white','black'],   
+        },
+        revo :{
+            image:'https://toyota-indus.com/wp-content/uploads/2022/10/Revo_Rocco_Grey-Graphite.png',
+            model:'2023',
+            seatcapacity:'5 person',
+            extra : 'loading area',
+            fuleType : 'petrol',
+            Transmission :'Type	Auto',
+            Mileage: "10 to 13 KM/LITER",
+            Wheel :	'18 in',
+            price: '11,959,000 PKR',  
+            color: ['red','white','black'],   
+        },
+        hilux :{
+            image:'https://imgd.aeplcdn.com/664x374/n/cw/ec/109265/hilux-exterior-left-front-three-quarter.jpeg?isig=0&q=80',
+            model:'2023',
+            seatcapacity:'5 person',
+            fuleType : 'petrol',
+            Transmission :'Type	Auto',
+            Mileage: "15 to 19 KM/LITER",
+            Wheel :	'14 in',
+            price: '3,200,000 PKR',  
+            color: ['red','white','black'],   
+        },
+        hiace :{
+            image:'https://cache2.pakwheels.com/system/car_generation_pictures/5443/original/hiace-cover.jpg?1615979645',
+            model:'2023',
+            seatcapacity:'5 person',
+            fuleType : 'petrol',
+            Transmission :'Type	Automatic',
+            Mileage: "10 to 14 KM/LITER",
+            Wheel :	'14 in',
+            price: '13,450,000 PKR',  
+            color: ['red','white','black']   
+        }
+    },
+    hyundai :{
+        tucson :{
+            image:'https://www.hyundai.com/content/dam/hyundai/in/en/data/find-a-car/Tucson/Gallery%20Section/pc/Tucson-gallery-4.jpg',
+            model:'2023',
+            seatcapacity:'5 person',
+            fuleType : 'petrol',
+            Transmission :'Type	Automatic',
+            Mileage: "10 to 14 KM/LITER",
+            Wheel :	'14 in',
+            price: '6,450,000 PKR',  
+            color: ['red','white','black']   
+        },
+        santafe : {
+            image:'https://www.motortrend.com/uploads/sites/10/2017/11/2016-hyundai-santafe-gls-suv-angular-front.png',
+            model:'2023',
+            seatcapacity:'5 person',
+            fuleType : 'petrol',
+            Transmission :'Type	Automatic',
+            Mileage: "10 to 14 KM/LITER",
+            Wheel :	'14 in',
+            price: '5,450,000 PKR',  
+            color: ['red','white','black']   
+        },
+        kona :{
+            image:'https://edgecast-img.yahoo.net/mysterio/api/9C35620A03BC87F11049BD180659F4024FED97DFBCB4E205E5E4789B910C5D46/autoblog/resizefill_w660_h372;quality_80;format_webp;cc_31536000;/https://s.aolcdn.com/commerce/autodata/images/USD30HYS061C021001.jpg',
+            model:'2023',
+            seatcapacity:'5 person',
+            fuleType : 'petrol',
+            Transmission :'Type	Automatic',
+            Mileage: "10 to 14 KM/LITER",
+            Wheel :	'18 in',
+            price: '7,870,000 PKR',  
+            color: ['red','white','black']   
+        },
+        palisade :{
+            image:'https://s7d1.scene7.com/is/image/hyundai/2023-palisade-calligraphy-sierra-burgundy:16-9?wid=640&hei=360&fmt=webp-alpha',
+            model:'2023',
+            seatcapacity:'5 person',
+            fuleType : 'petrol',
+            Transmission :'Type	Automatic',
+            Mileage: "10 to 14 KM/LITER",
+            Wheel :	'16 in',
+            price: '8,670,000 PKR',  
+            color: ['red','white','black']   
+        },
+        venue :{
+            image:'https://akm-img-a-in.tosshub.com/indiatoday/images/story/201905/Hyundai_Venue_Exterior.jpeg?size=690:388',
+            model:'2023',
+            seatcapacity:'5 person',
+            fuleType : 'petrol',
+            Transmission :'Type	Automatic',
+            Mileage: "10 to 14 KM/LITER",
+            Wheel :	'17 in',
+            price: '6,850,000 PKR',  
+            color: ['red','white','black']   
+        }
+        
+    }
+    
+}
+ 
+var maintain = document.getElementById('maintain')
+var brand = document.getElementById('brand')
+var model = document.getElementById('model')
+brand.innerHTML =`<option>select brand name</option>`
+model.innerHTML =`<option>select model name</option>`
+
+function randerall(){
+
+for ( var key in carlist){
+    brand.innerHTML+=`<option>${key}</option>`
+    for(var key1 in carlist[key]){
+        var carvalue = carlist[key][key1]
+        colorDiv = '';
+        for( i=0; i<carvalue.color.length; i++){
+            colorDiv +=`<div class="${carvalue.color[i]}"></div>`
+        //    console.log(carvalue.color[i])
+        }
+ 
+        maintain.innerHTML+= `
+        <div class="col-sm-12 col-md-6 col-lg-3 row1">
+            <div class="card cart p-2" style="width: 100%; ">
+                <img src=${carvalue.image}
+                    class="card-img-top" alt="...">
+                <div class="card-body" id='carDetails'>
+                <h5 class="card-title">${key1.toLocaleUpperCase()} (${carvalue.model})</h5>
+                <p>${carvalue.fuleType}</p>
+                <p>${carvalue.Transmission}</p>
+                <h5>${carvalue.price}</h5>
+                <p>Colors</p>
+                <div class="d-flex gap-2 al">${colorDiv}
+                </div>
+
+                </div>
+            </div>
+        </div>`
+    }
+
+
+}
+}
+randerall()
+
+function setModel(){
+//    console.log( brand.value)
+   
+   model.innerHTML=''
+   model.innerHTML =`<option>select model name</option>`
+
+   for(key in carlist[brand.value]){
+        //    console.log(key)
+           model.innerHTML+=`<option>${key}</option>`
+           
+   }
+
+
+}
+
+var carFull=document.getElementById('carFull')
+
+function findCar(){
+    maintain.style.display='none'
+
+    carFull.style.display='flex'
+    var impcar=carlist[brand.value][model.value]
+    carFull.innerHTML=`
+    <div class="card mb-8 no" >
+        <div class="row g-0">
+          <div class="col-md-4">
+            <img src="${impcar.image}" class="img-fluid rounded-start" alt="...">
+          </div>
+          <div class="col-md-8">
+            <div class="card-body">
+              <h2 class="card-title">${model.value.toUpperCase()}</h2>
+              <p class="card-text">(${impcar.model})</p>
+              <p class="card-text">${impcar.Transmission}</p>
+              <p class="card-text">${impcar.fuleType}</p>
+              <h4 class="card-text">${impcar.price}</h4>
+              <p>Colors</p>
+              <div class="d-flex gap-2 al">${colorDiv}
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>`
+}
+
+
+function clearall(){
+    maintain.style.display='flex'
+    carFull.style.display='none'
+    randerall()
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
